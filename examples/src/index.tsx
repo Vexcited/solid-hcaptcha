af3f03f2a4cee1548ec9ac2454b9454810dd77f0
Mikkel RINGAUD <mikkel@milescode.dev>
2022-05-22 21:15:15 +0200

    fix: update example to preview the component

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -1,5 +1,19 @@
 /* @refresh reload */
+import type { Component } from "solid-js";
 import { render } from "solid-js/web";
 
+import HCaptcha from "solid-hcaptcha";
 
-render(() => <App />, document.getElementById('root') as HTMLElement);
+const Main: Component = () => {
+  return (
+    <div>
+      <h1>solid-hcaptcha</h1>
+      <HCaptcha />
+    </div>
+  );
+}
+
+render(
+  () => <Main />,
+  document.getElementById("root") as HTMLDivElement
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import type { Component } from "solid-js";
 
 const HCaptcha: Component = () => {
-    return (
-        <div>Here is my captcha.</div>
-    );
+  return (
+    <div>Here is my captcha.</div>
+  );
 };
 
 export default HCaptcha;
