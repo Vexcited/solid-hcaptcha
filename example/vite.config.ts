93ad0aa4aee46e48213a1d176410bd12357e50ea
Mikkel RINGAUD <mikkel@milescode.dev>
2022-05-24 22:35:30 +0200

    fix(example): add env variables from global env

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
+/** Additional environment variables. */
+const viteEnv = {};
+
+/** Load variables from process environment variables. */
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith("VITE_")) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key];
+  }
+});
+
+
 export default defineConfig({
   plugins: [solidPlugin()],
-  
+  define: viteEnv,
+
   build: {
     target: "esnext",
     polyfillDynamicImport: false
